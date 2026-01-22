06de89ae837cb285e2a40a887a9263cdc36cece3
rajmahesh446-cell <rajmahesh446@gmail.com>
2026-01-22 11:39:47 +0530

    Create Atbash Cipher (Mirror Alphabet).sol

---
+++ Atbash Cipher (Mirror Alphabet).sol
@@ -0,0 +1,18 @@
+public class Atbash {
+    public static void main(String[] args) {
+        String message = "ABC";
+        String result = "";
+
+        for (char c : message.toCharArray()) {
+            // Subtract character from the sum of 'A' and 'Z' to mirror it
+            if (Character.isUpperCase(c)) {
+                result += (char) ('Z' - (c - 'A'));
+            } else {
+                result += c;
+            }
+        }
+        
+        System.out.println("Mirrored: " + result); 
+        // Output: ZYX
+    }
+}
